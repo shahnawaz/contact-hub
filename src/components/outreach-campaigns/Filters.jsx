abf312a37c4af6b54ff3b710742c45714008a2c4
Shahnawaz Ali <mr_shah@live.com>
2020-10-01 01:18:12 +0500

    Basic campaigns page added

---
+++ src/components/outreach-campaigns/Filters.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import Box from "@material-ui/core/Box";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+}));
+
+export const Filters = ({ activeFilters = {}, onChange }) => {
+    const classes = useStyles();
+    const [age, setAge] = React.useState('');
+
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };
+
+    return (
+        <Box display="flex" justifyContent="space-between">
+            <Box>Filter Campaigns</Box>
+            <FormControl variant="outlined" className={classes.formControl}>
+                <InputLabel id="demo-simple-select-outlined-label">Select User</InputLabel>
+                <Select
+                    labelId="demo-simple-select-outlined-label"
+                    id="demo-simple-select-outlined"
+                    value={age}
+                    onChange={handleChange}
+                    label="Select User"
+                >
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl>
+            <FormControl variant="outlined" className={classes.formControl}>
+                <InputLabel id="demo-simple-select-outlined-label">Select Account</InputLabel>
+                <Select
+                    labelId="demo-simple-select-outlined-label"
+                    id="demo-simple-select-outlined"
+                    value={age}
+                    onChange={handleChange}
+                    label="Select Account"
+                >
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl>
+            <FormControl variant="outlined" className={classes.formControl}>
+                <InputLabel id="demo-simple-select-outlined-label">Active</InputLabel>
+                <Select
+                    labelId="demo-simple-select-outlined-label"
+                    id="demo-simple-select-outlined"
+                    value={age}
+                    onChange={handleChange}
+                    label="Active"
+                >
+                    <MenuItem value="">
+                        <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl>
+        </Box>
+    );
+};
--- src/pages/OutreachCampaigns.jsx
+++ src/pages/OutreachCampaigns.jsx
@@ -1,8 +1,42 @@
 import React from 'react';
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
+
+import { Filters } from "../components/outreach-campaigns/Filters";
+import { CampaignsList } from "../components/outreach-campaigns/CampaignsList";
 
 const OutreachCampaigns = (props) => {
+    const [activeFilters, setActiveFilters] = React.useState({});
+
+    const onFiltersChange = (updatedFilters) => {
+        // TODO:
+        // 1. set active filters
+        // 2. call Get campaigns API with updated filters
+    };
+
+    const onAddNew = () => {
+        // TODO:
+    };
+
     return (
-        <div>Outreach Campaigns</div>
+        <Container>
+
+            {/* Main Heading */}
+            <Box display="flex" justifyContent="space-between">
+                <Box>Outreach Campaigns</Box>
+                <Button variant="contained" color="primary" onClick={onAddNew}>
+                    Primary
+                </Button>
+            </Box>
+
+            {/* Filters */}
+            <Filters activeFilters={activeFilters} onChange={onFiltersChange} />
+
+            {/* Campaigns List */}
+            <CampaignsList />
+
+        </Container>
     );
 };
 
